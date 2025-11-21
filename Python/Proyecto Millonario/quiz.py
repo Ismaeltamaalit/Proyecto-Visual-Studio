87c7ea776d4c9f027726326c9d293520db09ae9e
daw2-2516 <ismatamaalit@gmail.com>
2025-11-21 17:17:00 +0100

    primer prototipo de proyecto

---
+++ Python/Proyecto Millonario/quiz.py
@@ -0,0 +1,45 @@
+def preguntas():
+    pregunta1="¿De que color es el cielo?"
+    pregunta2="¿Cuantas extremidades tiene una persona"
+    pregunta3="¿Cuando se adoptó el Euro en España"
+    pregunta4="¿Cual es la ultima consola de Sony?"
+    pregunta5="¿Que personaje es el más importante de Nintendo?"
+    
+    pregunta6="¿Quien fue el primer emperador de Roma?"
+    pregunta7="¿Que dos reyes de dos paises unieron las coronas para formar España?"
+    pregunta8="¿Cuando se lanzó el primero juego de Zelda?"
+    pregunta9="¿Cuantos mundiales de futbol ganó España"
+    pregunta10="¿Cuando se ganó?"
+    
+    pregunta11="¿Cual es el videojuego mas vendido de la historia?"
+    pregunta12="¿Cual es la saga de videojuegos más importante de Xbox?"
+    pregunta13="¿Que cientifico propone la primera base matématica sobre los agujeros negros?"
+    pregunta14="¿Cual fue la primera consola de videojuegos en salir al mercado?"
+    pregunta15="¿Cual es la empresa del mundo gaming más antigua del mundo?"
+    
+    
+    """
+    Dice el profesor que el proyecto tiene que ir en tres arrays
+    distintos
+    """
+    
+    
+    
+    
+    respuestas_correctas = {
+    "pregunta1": "azul",
+    "pregunta2": "4",
+    "pregunta3": "2002",
+    "pregunta4": "PlayStation 5",
+    "pregunta5": "Mario",
+    "pregunta6": "Augusto",
+    "pregunta7": "Castilla, y Aragón",  
+    "pregunta8": "1986",
+    "pregunta9": "1",
+    "pregunta10": "2010",
+    "pregunta11": "Minecraft",
+    "pregunta12": "Halo",
+    "pregunta13": "Karl Schwarzschild",
+    "pregunta14": "Magnavox Odyssey",
+    "pregunta15": "Nintendo"
+}
