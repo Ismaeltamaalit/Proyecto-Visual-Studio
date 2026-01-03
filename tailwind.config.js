97281b683d1d0a90fa94fcd8a60ef0fe83a93273
Ismaeltamaalit <ismatamaalit@gmail.com>
2026-01-03 11:48:02 +0100

    sdfsfdsfsfs

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./src/**/*.{js,ts,jsx,tsx}"], // Aquí le decimos que vigile los HTML de la raíz y cualquier JS en la carpeta src
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
